f5444ae5de8830f1ad305924bc4fdad85afa0f0e
IdreesMon <151092705+IdreesMon@users.noreply.github.com>
2024-04-16 11:48:18 -0400

    Week 4

---
+++ Week 4 Challenge/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace Week4_Challenge
+{
+    class Program
+    {
+
+        
+        struct Point
+        {
+            //FIRST DEFINE THE STRUCT
+            public int X;
+            public int Y;
+            
+            //CONSTUCTOR
+             public Point(int x, int y)
+            {
+                X = x;
+                Y =y;
+            }
+
+        }
+
+        GetRandomPoint();
+        {
+            Random random = new Random();
+            //
+            Point point = new Point();
+            //ASSIGN RANGES TO X AND Y
+            minX = -100;
+            maxX = 100;
+            minY = -100;
+            maxY = 100;
+            
+            point.x = random.Next(minX, maxX);
+            point.y = random.Next(minY, maxY);
+            //RETURN RANDOM INT TO CALLER
+            return point;
+        }
+           
+           //static means you will return a value
+        static void Main(string[] args)
+        {
+
+            GetRandomPoint();
+            //CALL FUNCTION
+            Point point = GetRandomPoint();
+            //PRINT OUT RANDOM POINT
+            Console.WriteLine($"Random Point: {point.X}, {point.Y}");
+
+
+    
+
+
+
+        }     
+    }
+}
