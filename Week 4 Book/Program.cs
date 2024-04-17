c57e081dcd8ad5891625d3349d2348f98fdda9c4
IdreesMon <151092705+IdreesMon@users.noreply.github.com>
2024-04-17 14:16:25 -0400

    Structs

---
+++ Week 4 Book/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Book
+{
+    public struct BookItemStruct
+    {
+        public string Title;
+        public string AuthFirst;
+        public string AuthLast;
+        public string Email;
+        public int DatePublic;
+        public int Price;
+        public int Pages;
+
+        public BookItemStruct(string title, string authFirst, string authLast, string email, int datePublic, int price, int pages)
+        {
+            Title = title;
+            AuthFirst = authFirst;
+            AuthLast = authLast;
+            Email = email;
+            DatePublic = datePublic;
+            Price = price;
+            Pages = pages;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookItemStruct newBook = new BookItemStruct(
+                "The Great Gatsby",
+                "F. Scott",
+                "Fitzgerald",
+                "x@gmail.com",
+                1925,
+                10,
+                180
+            );
+
+            Console.WriteLine(newBook.Title);
+        }
+
+}
+}
