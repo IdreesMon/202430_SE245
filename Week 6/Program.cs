56dac4677c86b15627bf2189b47072069275a50e
IdreesMon <151092705+IdreesMon@users.noreply.github.com>
2024-04-30 11:11:31 -0400

    Week6

---
+++ Week 6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Week6Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           static string ReverseString(string text) //Main Function
+
+        {  
+            string reversed = "";
+            for(int i = text.Length - 1; i >= 0; i--)
+            {
+        
+                //Console.Write(text[i]);
+                reversed += text[i];
+            }
+            return reversed;
+        }
+           
+        }
+    }
+}
